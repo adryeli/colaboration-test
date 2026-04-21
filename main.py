156f8262e57b5896dea2091840b8d1deacdaf85a
drojas7-7 <270030489+drojas-7u7@users.noreply.github.com>
2026-04-21 13:00:31 +0200

    feat: añadir función de resta

--- main.py
+++
@@ -1,5 +0,0 @@
-# Archivo creado por el equipo: David (Coder B)
-
-def resta(a, b):
-    """Función para restar dos números"""
-    return a - b
---
+++ math_operations.py
@@ -0,0 +1,5 @@
+# Archivo editado por el equipo: David (Coder B)
+
+def resta(a, b):
+    """Función para restar dos números"""
+    return a - b
